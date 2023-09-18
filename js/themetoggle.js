ff751ac530fce90ffda9bf0543ebf6de37fdb775
rezaarezvan <rezvanr2378@gmail.com>
2023-09-18 14:26:09 +0200

    Update: theme

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -1,11 +1,11 @@
 function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled=false;
+        document.getElementById("darkModeStyle").disabled = false;
         document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
         feather.replace()
     } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled=true;
+        document.getElementById("darkModeStyle").disabled = true;
         document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
         feather.replace()
     }
