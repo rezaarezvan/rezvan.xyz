09fdc3fac77c8a45a0fc01ee132686e321e4e85f
rezaarezvan <rezvanr2378@gmail.com>
2024-07-08 00:38:41 +0200

    kinda big overhaul?

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -9,15 +9,11 @@ function setTheme(mode) {
         darkModeStyle.disabled = false;
         lightSyntaxStyle.disabled = true;
         darkSyntaxStyle.disabled = false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
     } else if (mode === "light") {
         darkModeStyle.disabled = true;
         lightSyntaxStyle.disabled = false;
         darkSyntaxStyle.disabled = true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
     }
-
-    feather.replace();
 }
 
 function toggleTheme() {
